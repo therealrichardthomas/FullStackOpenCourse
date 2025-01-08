2218294a85a8050c91bb2f545364a949f3d36f5d
Richard Thomas <richuthomasp@gmail.com>
2025-01-08 08:01:01 +0530

    part 3-backend

--- Part 2/frontend/src/services/notes.js
+++ Part 2/frontend/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 // returns all the contents in the notes collection
 const getAll = () => {
