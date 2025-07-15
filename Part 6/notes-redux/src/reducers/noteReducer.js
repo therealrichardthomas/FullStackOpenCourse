e38456ca9ae474ccb9afb6fb32b4adead6ec8507
Richard Thomas <richuthomasp@gmail.com>
2025-07-15 10:37:51 -0400

    Part 6-1

---
+++ Part 6/notes-redux/src/reducers/noteReducer.js
@@ -0,0 +1,39 @@
+const noteReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.payload]
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id
+      const ogNote = state.find(note => note.id === id)
+      const changedNote = {
+        ...ogNote,
+        important: !ogNote.important
+      }
+      return state.map(note => note.id !== id ? note : changedNote)
+    }
+    default:
+      return state
+  }
+}
+
+const generateId = () => Number(Math.random() * 1000000).toFixed(0)
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
+export default noteReducer;
