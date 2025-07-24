66fac90f1f7042407d1e0e6e30042a67c5aca02f
Richard Thomas <richuthomasp@gmail.com>
2025-07-24 08:07:30 -0400

    Part 7C

--- Part 7/notes-router-app/src/App.jsx
+++ Part 7/notes-router-app/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 import { 
   // BrowserRouter as Router,
   Routes, Route, Link,
   useNavigate, Navigate,
   useMatch
 } from 'react-router-dom'
+// import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap'
+import { 
+  Container,
+  TableContainer, Table, TableBody, TableRow, TableCell, Paper,
+  TextField, Button,
+  Alert,
+  AppBar, Toolbar, IconButton
+} from '@mui/material'
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
 
 const Home = () => (
   <div> 
@@ -16,13 +34,39 @@ const Home = () => (
 const Notes = ({notes}) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note => 
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    {/* react-bootstrap */}
+    {/* <Table striped>
+      <tbody>
+        {notes.map(note => 
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}  
+      </tbody>
+    </Table> */}
+
+    {/* MaterialUI */}
+    <TableContainer component={Paper}>
+      <Table>
+          <TableBody>
+            {notes.map(note => (
+              <TableRow key={note.id}>
+                <TableCell>
+                  <Link to={`/notes/${note.id}`}>{note.content}</Link>
+                </TableCell>
+                <TableCell>
+                  {note.user}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 )
 const Note = ({ note }) => {
@@ -36,7 +80,15 @@ const Note = ({ note }) => {
 }
 
 const Users = () => (
-  <div> <h2>Users</h2> </div>
+  <div>
+    <h2>Users</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
+  </div>
+  
 )
 const Login = (props) => {
   const navigate = useNavigate()
@@ -50,14 +102,32 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
+      {/* react-bootstrap */}
+      {/* <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username: </Form.Label>
+          <Form.Control type='text' name='username' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>password: </Form.Label>
+          <Form.Control type='password' />
+          <Button variant='primary' type="submit">login</Button>
+        </Form.Group>
+      </Form> */}
+
+      {/* MaterialUI */}
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          <TextField label="username" />
         </div>
         <div>
-          password: <input type='password' />
+          <TextField label="password" type='password'/>
         </div>
-        <button type="submit">login</button>
+        <div>
+          <Button variant='contained' color='primary' type='submit'>
+            login
+          </Button>
+        </div>
       </form>
     </div>
   )
@@ -86,45 +156,112 @@ const App = () => {
   ])
 
   const [user, setUser] = useState(null)
-
+  const [message, setMessage] = useState(null)
+  
+  const match = useMatch('notes/:id')
+  const note = match ? notes.find(n => n.id === Number(match.params.id)) : null
+  
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000);
   }
 
   const padding = {
     padding: 5
   }
 
-  const match = useMatch('notes/:id')
-  const note = match ? notes.find(n => n.id === Number(match.params.id)) : null
 
   return (
-    <div>
-        <div>
-          {/* Link modifies the address bar */}
-          <Link style={padding} to="/">home</Link> 
-          <Link style={padding} to="/notes">notes</Link>
-          <Link style={padding} to="/users">users</Link>
-          {user 
-            ? <em>{user} logged in</em>
-            : <Link style={padding} to="/login">login</Link>
-          }
-        </div>
+    // react-bootstrap framework
+    // <div className='container'>
+    //   {(message && 
+    //     <Alert variant='success'>
+    //       {message}
+    //     </Alert>
+    //   )}
+    //     <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+    //       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+    //       <Navbar.Collapse id='responsive-navbar-nav'>
+    //         <Nav className='me-auto'>
+    //           <Nav.Link href="#" as="span">
+    //             <Link style={padding} to="/">home</Link> 
+    //           </Nav.Link>
+    //           <Nav.Link href="#" as="span">
+    //             <Link style={padding} to="/notes">notes</Link> 
+    //           </Nav.Link>
+    //           <Nav.Link href="#" as="span">
+    //             <Link style={padding} to="/users">users</Link> 
+    //           </Nav.Link>
+    //           <Nav.Link href="#" as="span">
+    //             {user 
+    //               ? <em style={padding}>{user} logged in</em>
+    //               : <Link style={padding} to="/login">login</Link>
+    //             }
+    //           </Nav.Link>
+    //         </Nav>
+    //       </Navbar.Collapse>
+    //     </Navbar>
 
-        {/* Routes is set up to render the FIRST component that matches the URL */}
-        <Routes>
-          {/* components are rendered based on the URL of the browser */}
-          <Route path="/notes/:id" element={<Note note={note}/>} />
-          <Route path="/notes" element={<Notes notes={notes}/>} />
-          <Route path="/users" element={user ? <Users /> : <Navigate replace to ="/login"/>} />
-          <Route path="/login" element={<Login onLogin={login}/>} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+    //     {/* Routes is set up to render the FIRST component that matches the URL */}
+    //     <Routes>
+    //       {/* components are rendered based on the URL of the browser */}
+    //       <Route path="/notes/:id" element={<Note note={note}/>} />
+    //       <Route path="/notes" element={<Notes notes={notes}/>} />
+    //       <Route path="/users" element={user ? <Users /> : <Navigate replace to ="/login"/>} />
+    //       <Route path="/login" element={<Login onLogin={login}/>} />
+    //       <Route path="/" element={<Home />} />
+    //     </Routes>
+    //   <footer>
+    //     <br />
+    //     <i>Note app, Department of Computer Science 2024</i>
+    //   </footer>
+    // </div>
+    
+    // MaterialUI framework
+    <Container>
+      {(message && 
+        <Alert severity='success'>
+          {message}
+        </Alert>
+      )}
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton edge='start' color="inherit" aria-label='menu'></IconButton>
+          <Button color="inherit" component={Link} to='/'>
+            home
+          </Button>
+          <Button color="inherit" component={Link} to='/notes'>
+            notes
+          </Button>
+          <Button color="inherit" component={Link} to='/users'>
+            users
+          </Button>
+          {user
+            ? <em>{user} logged in</em>
+            : <Button color='inherit' component={Link} to='/login'>
+                login
+              </Button>
+          }
+        </Toolbar>
+      </AppBar>
+
+      {/* Routes is set up to render the FIRST component that matches the URL */}
+      <Routes>
+        {/* components are rendered based on the URL of the browser */}
+        <Route path="/notes/:id" element={<Note note={note}/>} />
+        <Route path="/notes" element={<Notes notes={notes}/>} />
+        <Route path="/users" element={user ? <Users /> : <Navigate replace to ="/login"/>} />
+        <Route path="/login" element={<Login onLogin={login}/>} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       <footer>
         <br />
         <i>Note app, Department of Computer Science 2024</i>
       </footer>
-    </div>
+    </Container>
   )
 }
 
