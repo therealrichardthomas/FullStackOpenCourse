6cec4406e6bc72ee7c25dd9fa4124efe082b0752
Richard Thomas <richuthomasp@gmail.com>
2025-07-25 08:21:38 -0400

    Part 7d

---
+++ Part 7/webpack/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import PromisePolyfill from 'promise-polyfill'
+import './index.css'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(res => setNotes(res.data))
+  }, [url])
+  return notes
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>press</button>
+      <div>{notes.length} notes on server {BACKEND_URL}</div>
+    </div>
+  )
+}
+
+export default App
